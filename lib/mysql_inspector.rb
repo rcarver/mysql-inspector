3ce330e1833c064b740d0513236fd12640c1945f
Ryan Carver <ryan@ryancarver.com>
2012-02-18 23:55:20 -0800

    cleanup

--- lib/mysql_inspector.rb
+++ lib/mysql_inspector.rb
@@ -16,7 +16,6 @@ require "mysql_inspector/diff"
 require "mysql_inspector/dump"
 require "mysql_inspector/grep"
 require "mysql_inspector/table"
-require "mysql_inspector/utils"
 
 module MysqlInspector
 
